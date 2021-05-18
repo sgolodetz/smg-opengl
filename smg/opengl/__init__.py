b2d5681dd2620ebf3fad3b67d0b962f5396a9cc9
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-18 13:03:28 +0100

    Move OpenGLMeshRenderer to smg.opengl.

--- smg/opengl/__init__.py
+++ smg/opengl/__init__.py
@@ -2,6 +2,7 @@ from .camera_renderer import CameraRenderer
 from .opengl_framebuffer import OpenGLFrameBuffer
 from .opengl_image_renderer import OpenGLImageRenderer
 from .opengl_matrix_context import OpenGLMatrixContext
+from .opengl_mesh_renderer import OpenGLMeshRenderer
 from .opengl_util import OpenGLUtil
 from .shape_renderer import ShapeRenderer
 from .triangle_mesh import TriangleMesh
