965bf0c0a35d000bc8f084a400e7fb83ea51bea5
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-21 14:21:16 +0100

    Introduce smg-rotorsim.

--- smg/opengl/__init__.py
+++ smg/opengl/__init__.py
@@ -2,7 +2,6 @@ from .camera_renderer import CameraRenderer
 from .opengl_framebuffer import OpenGLFrameBuffer
 from .opengl_image_renderer import OpenGLImageRenderer
 from .opengl_matrix_context import OpenGLMatrixContext
-from .opengl_scene_renderer import OpenGLSceneRenderer
 from .opengl_trimesh import OpenGLTriMesh
 from .opengl_util import OpenGLUtil
 from .shape_renderer import ShapeRenderer
