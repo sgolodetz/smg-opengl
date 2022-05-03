8e4a799b42f5a64e9cb05a3dd9a292d25d7a202a
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-03 13:24:57 +0100

    Introduce OpenGLDepthTestContext.

--- smg/opengl/__init__.py
+++ smg/opengl/__init__.py
@@ -1,4 +1,5 @@
 from .camera_renderer import CameraRenderer
+from .opengl_depth_test_context import OpenGLDepthTestContext
 from .opengl_framebuffer import OpenGLFrameBuffer
 from .opengl_image_renderer import OpenGLImageRenderer
 from .opengl_lighting_context import OpenGLLightingContext
